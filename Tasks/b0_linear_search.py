37e4d3397727f6eab897a1a01ff088cc54265a7e
TILTIL <x2g.tillo.g@inbox.ru>
2019-12-19 18:34:07 +0300

    numpy

--- Tasks/b0_linear_search.py
+++ Tasks/b0_linear_search.py
@@ -2,6 +2,7 @@
 This module implements some functions based on linear search algo
 """
 from typing import Sequence
+import numpy as np
 
 
 def min_search(arr: Sequence) -> int:
@@ -13,3 +14,23 @@ def min_search(arr: Sequence) -> int:
 	"""
 	print(arr)
 	return -1
+
+def finding_elem(find_elem, array):
+
+	for i, elem in enumerate(array):
+		if i ==
+	index = None
+	pass
+
+	return index
+
+if __name__ == '__main__':
+
+	n = 1000
+	array = np.arange(n)
+	find_elem = np.ramdom.choice (array)
+	np.random.shuffle(array)
+
+	print (finding_elem(find_elem, array))
+
+
