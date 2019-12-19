6ad13a94b7050e0ff3bd13e19477b0da85743ea9
TILTIL <x2g.tillo.g@inbox.ru>
2019-12-19 20:26:17 +0300

    тесты доделать бы

--- Tasks/b0_linear_search.py
+++ Tasks/b0_linear_search.py
@@ -15,22 +15,22 @@ def min_search(arr: Sequence) -> int:
 	print(arr)
 	return -1
 
-def finding_elem(find_elem, array):
-
-	for i, elem in enumerate(array):
-		if i ==
-	index = None
-	pass
-
-	return index
-
-if __name__ == '__main__':
-
-	n = 1000
-	array = np.arange(n)
-	find_elem = np.ramdom.choice (array)
-	np.random.shuffle(array)
-
-	print (finding_elem(find_elem, array))
+# def finding_elem(find_elem, array):
+#
+# 	for i, elem in enumerate(array):
+#
+# 	index = None
+# 	pass
+#
+# 	return index
+#
+# if __name__ == '__main__':
+#
+# 	n = 1000
+# 	array = np.arange(n)
+# 	find_elem = np.ramdom.choice (array)
+# 	np.random.shuffle(array)
+#
+# 	print (finding_elem(find_elem, array))
 
 
--- Tasks/b1_binary_search.py
+++ Tasks/b1_binary_search.py
@@ -1,4 +1,5 @@
 from typing import Any, Sequence, Optional
+from random import randint
 
 
 def binary_search(elem: Any, arr: Sequence) -> Optional[int]:
@@ -9,5 +10,62 @@ def binary_search(elem: Any, arr: Sequence) -> Optional[int]:
 	:param arr: array where element is to be found
 	:return: Index of element if it's presented in the arr, None otherwise
 	"""
+
+	arr.sort()
 	print(elem, arr)
-	return None
+
+	mid = len(arr) // 2
+	low = 0
+	high = len(arr) - 1
+
+	flag = 0
+
+	while arr[mid] != elem and low <= high:
+		if elem > arr[mid]:
+			low = mid + 1
+		else:
+			high = mid - 1
+		mid = (low + high) // 2
+
+		#if elem <
+
+	for i in arr:
+		print(i)
+		if i != elem:
+			return None
+		else:
+			return mid
+
+
+	return mid
+
+
+def test ():
+
+	# Создание списка,
+	# его сортировка по возрастанию
+	# и вывод на экран
+	a = []
+	for i in range(15):
+		a.append(randint(1, 50))
+	a.sort()
+	print(a)
+
+	# искомое число
+	value = int(input())
+
+	mid = len(a) // 2
+	low = 0
+	high = len(a) - 1
+
+	while a[mid] != value and low <= high:
+		if value > a[mid]:
+			low = mid + 1
+		else:
+			high = mid - 1
+		mid = (low + high) // 2
+
+	if low > high:
+		print("No value")
+	else:
+		print("ID =", mid)
